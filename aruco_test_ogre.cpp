676fe77aaa3f6f3db8b480ac6ea26a623e4373ad
sharganov <sharganov@sharganov.comp>
2017-01-13 16:53:13 +0300

    Version 0.1; Merge files, clean up

--- aruco_test_ogre.cpp
+++ aruco_test_ogre.cpp
@@ -1,5 +1,5 @@
 /*****************************
-Copyright 2011 Rafael Muñoz Salinas. All rights reserved.
+Copyright 2011 Rafael Muñoz Salinas, Sharganov Artem, Antropov Igor. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are
 permitted provided that the following conditions are met:
@@ -11,7 +11,7 @@ permitted provided that the following conditions are met:
       of conditions and the following disclaimer in the documentation and/or other materials
       provided with the distribution.
 
-THIS SOFTWARE IS PROVIDED BY Rafael Muñoz Salinas ''AS IS'' AND ANY EXPRESS OR IMPLIED
+THIS SOFTWARE IS PROVIDED BY Rafael Muñoz Salinas, Sharganov Artem, Antropov Igor ''AS IS'' AND ANY EXPRESS OR IMPLIED
 WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Rafael Muñoz Salinas OR
 CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
@@ -23,7 +23,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 The views and conclusions contained in the software and documentation are those of the
 authors and should not be interpreted as representing official policies, either expressed
-or implied, of Rafael Muñoz Salinas.
+or implied, of Rafael Muñoz Salinas, Sharganov Artem, Antropov Igor.
 ********************************/
 
 #include <iostream>
@@ -63,7 +63,7 @@ float TheMarkerSize=1;
 aruco::MarkerDetector TheMarkerDetector;
 std::vector<aruco::Marker> TheMarkers;
 
-cv::Mat camMatrix, distCoeffs;
+
 const char* keys  =
         "{d        |       | dictionary: DICT_4X4_50=0, DICT_4X4_100=1, DICT_4X4_250=2,"
         "DICT_4X4_1000=3, DICT_5X5_50=4, DICT_5X5_100=5, DICT_5X5_250=6, DICT_5X5_1000=7, "
@@ -83,90 +83,8 @@ cv::Ptr<cv::aruco::Dictionary> dictionary;
 int initOgreAR(aruco::CameraParameters camParams, unsigned char* buffer, std::string resourcePath="");
 bool readParameters(int argc, char** argv);
 
-static bool readCameraParameters(string filename, cv::Mat &camMatrix, cv::Mat &distCoeffs) {
-    cv::FileStorage fs("camera.yml", cv::FileStorage::READ);
-    if(!fs.isOpened())
-        return false;
-    fs["camera_matrix"] >> camMatrix;
-    fs["distortion_coefficients"] >> distCoeffs;
-    return true;
-}
 
-
-void OgreGetPoseParameters(cv::Vec3d Tvec, cv::Vec3d Rvec, double position[3], double orientation[4]) throw(cv::Exception) {
-
-
-    // calculate position vector
-    position[0] = -Tvec[0];
-    position[1] = -Tvec[1];
-    position[2] = +Tvec[2];
-
-    // now calculare orientation quaternion
-    cv::Mat Rot(3, 3, CV_32FC1);
-    cv::Rodrigues(Rvec, Rot);
-
-    // calculate axes for quaternion
-    double stAxes[3][3];
-    // x axis
-    stAxes[0][0] = -Rot.at< double >(0, 0);
-    stAxes[0][1] = -Rot.at< double >(1, 0);
-    stAxes[0][2] = +Rot.at< double >(2, 0);
-    // y axis
-    stAxes[1][0] = -Rot.at< double >(0, 1);
-    stAxes[1][1] = -Rot.at< double >(1, 1);
-    stAxes[1][2] = +Rot.at< double >(2, 1);
-    // for z axis, we use cross product
-    stAxes[2][0] = stAxes[0][1] * stAxes[1][2] - stAxes[0][2] * stAxes[1][1];
-    stAxes[2][1] = -stAxes[0][0] * stAxes[1][2] + stAxes[0][2] * stAxes[1][0];
-    stAxes[2][2] = stAxes[0][0] * stAxes[1][1] - stAxes[0][1] * stAxes[1][0];
-
-    // transposed matrix
-    double axes[3][3];
-    axes[0][0] = stAxes[0][0];
-    axes[1][0] = stAxes[0][1];
-    axes[2][0] = stAxes[0][2];
-
-    axes[0][1] = stAxes[1][0];
-    axes[1][1] = stAxes[1][1];
-    axes[2][1] = stAxes[1][2];
-
-    axes[0][2] = stAxes[2][0];
-    axes[1][2] = stAxes[2][1];
-    axes[2][2] = stAxes[2][2];
-
-    // Algorithm in Ken Shoemake's article in 1987 SIGGRAPH course notes
-    // article "Quaternion Calculus and Fast Animation".
-    double fTrace = axes[0][0] + axes[1][1] + axes[2][2];
-    double fRoot;
-
-    if (fTrace > 0.0) {
-        // |w| > 1/2, may as well choose w > 1/2
-        fRoot = sqrt(fTrace + 1.0); // 2w
-        orientation[0] = 0.5 * fRoot;
-        fRoot = 0.5 / fRoot; // 1/(4w)
-        orientation[1] = (axes[2][1] - axes[1][2]) * fRoot;
-        orientation[2] = (axes[0][2] - axes[2][0]) * fRoot;
-        orientation[3] = (axes[1][0] - axes[0][1]) * fRoot;
-    } else {
-        // |w| <= 1/2
-        static unsigned int s_iNext[3] = {1, 2, 0};
-        unsigned int i = 0;
-        if (axes[1][1] > axes[0][0])
-            i = 1;
-        if (axes[2][2] > axes[i][i])
-            i = 2;
-        unsigned int j = s_iNext[i];
-        unsigned int k = s_iNext[j];
-
-        fRoot = sqrt(axes[i][i] - axes[j][j] - axes[k][k] + 1.0);
-        double *apkQuat[3] = {&orientation[1], &orientation[2], &orientation[3]};
-        *apkQuat[i] = 0.5 * fRoot;
-        fRoot = 0.5 / fRoot;
-        orientation[0] = (axes[k][j] - axes[j][k]) * fRoot;
-        *apkQuat[j] = (axes[j][i] + axes[i][j]) * fRoot;
-        *apkQuat[k] = (axes[k][i] + axes[i][k]) * fRoot;
-    }
-}
+void OgreGetPoseParameters(cv::Vec3d Tvec, cv::Vec3d Rvec, double position[3], double orientation[4]) throw(cv::Exception);
 
 void usage()
 {
@@ -178,17 +96,41 @@ void usage()
 }
 
 
+int decideOrientation(const cv::Mat& flow)
+{
+    int fx = flow.at<cv::Point2f>(0,0).x;
+    int fy = flow.at<cv::Point2f>(0,0).y;
+
+    for(int y =0; y < flow.rows; y++)
+    {
+        for(int x=0; x <  flow.cols; x++)
+        {
+            fx += flow.at<cv::Point2f>(y,x).x;
+
+            fy += flow.at<cv::Point2f>(y,x).y;
+        }
+    }
+    fx = fx / flow.cols;
+    fy = fy / flow.rows;
+
+
+    return fx;
+
+}
+
 int main(int argc, char** argv)
 {
 
-    cv::CommandLineParser parser(argc, argv, keys);
+    cv::Size size(160, 120);
+    cv::Mat prvs, next; //current frame
+
+    int curCounter = 0;
+    int prevCounter = 0;
+    int curValue = 0;
+    int flags = 0;
+    int temp = 0;
 
     dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME(0));
-
-    bool readOk = readCameraParameters(parser.get<string>("c"), camMatrix, distCoeffs);
-
-    if(!readOk) throw cv::Exception();
-
     /// READ PARAMETERS
     if(!readParameters(argc, argv))
         return false;
@@ -201,62 +143,89 @@ int main(int argc, char** argv)
     /// CAPTURE FIRST FRAME
     TheVideoCapturer.grab();
     TheVideoCapturer.retrieve ( newImg );
+
+    // for optical flow
+    cv::resize(newImg, prvs, size);
+    cvtColor(prvs, prvs, CV_BGR2GRAY);
+    //
+
     cv::undistort(newImg,TheInputImageUnd,CameraParams.CameraMatrix,CameraParams.Distorsion);
 
-
-
     /// INIT OGRE
-    //initOgreAR(CameraParamsUnd, TheInputImageUnd.ptr<uchar>(0));
-
     initOgreAR(CameraParamsUnd, newImg.ptr<uchar>(0));
-
 
     while (TheVideoCapturer.grab())
     {
 
-        /// READ AND UNDISTORT IMAGE
-        TheVideoCapturer.retrieve ( newImg );
-        //cv::undistort(newImg,TheInputImageUnd,CameraParams.CameraMatrix,CameraParams.Distorsion);
+        TheVideoCapturer.retrieve(newImg);
 
+        cv::resize(newImg, next, size);
+        cv::cvtColor(next, next, CV_BGR2GRAY);
+
+        cv::Mat flow;
+        cv::calcOpticalFlowFarneback(prvs, next, flow, 0.5, 1, 15, 3, 5, 1.2, 0);
+
+        curCounter += curValue = decideOrientation(flow);
+
+        if (curValue == 0){
+            flags++;
+        }
+
+        temp = curCounter - prevCounter;
+        if(abs(temp) > 100){
+            if (flags >= 5){
+                if (temp < 0){
+
+                    for(int i=0; i < MAX_MARKERS; i++)
+                    {
+                        ogreNode[i]->scale(2.0,2.0,2.0);
+                    }
+                    flags = 0;
+                    prevCounter = curCounter;
+                } else {
+                    printf("VPRAVO");
+                    for(int i=0; i < MAX_MARKERS; i++) ogreNode[i]->scale(0.5,0.5,0.5);
+                    flags = 0;
+                    prevCounter = curCounter;
+                }
+            }
+        }
+        else {
+            flags = 0;
+        }
+
+        printf("cur = %i, prev = %i, curVal = %i, flags = %i  \n", curCounter, prevCounter, curValue, flags);
+        prvs = next.clone();
 
         vector< int > ids;
         vector< vector< cv::Point2f > > corners;
 
         cv::aruco::detectMarkers(newImg, dictionary, corners, ids);
 
-
         vector< cv::Vec3d> rvecs, tvecs;
 
         if(ids.size() > 0)
-        {
-
-            cv::aruco::estimatePoseSingleMarkers(corners, 0.057, camMatrix, distCoeffs, rvecs, tvecs);
-
-        }
+            cv::aruco::estimatePoseSingleMarkers(corners, 0.057, CameraParams.CameraMatrix, CameraParams.Distorsion, rvecs, tvecs);
 
         double position[3], orientation[4];
 
         /// UPDATE SCENE
-        int i=0;
+        unsigned i=0;
 
         for(i=0; i < ids.size(); i++)
         {
-
-
             OgreGetPoseParameters(tvecs[i], rvecs[i], position, orientation);
 
             ogreNode[i]->setPosition( position[0], position[1], position[2] );
             ogreNode[i]->setOrientation( orientation[0], orientation[1], orientation[2], orientation[3]  );
             ogreNode[i]->setVisible(true);
-
         }
-
 
         if(ids.size() > 0) {
             cv::aruco::drawDetectedMarkers(newImg, corners, ids);
 
             for(unsigned int i = 0; i < ids.size(); i++)
-                cv::aruco::drawAxis(newImg, camMatrix, distCoeffs, rvecs[i], tvecs[i],
+                cv::aruco::drawAxis(newImg, CameraParams.CameraMatrix, CameraParams.Distorsion, rvecs[i], tvecs[i],
                                     0.057 * 0.5f);
         }
 
@@ -303,7 +272,7 @@ bool readParameters(int argc, char** argv)
         return false;
     }
     // read input video
-    TheVideoCapturer.open(1);
+    TheVideoCapturer.open(0);
 
     if (!TheVideoCapturer.isOpened())
     {
@@ -436,10 +405,80 @@ int initOgreAR(aruco::CameraParameters camParams, unsigned char* buffer, std::st
 }
 
 
+void OgreGetPoseParameters(cv::Vec3d Tvec, cv::Vec3d Rvec, double position[3], double orientation[4]) throw(cv::Exception)
+{
 
+    // calculate position vector
+    position[0] = -Tvec[0];
+    position[1] = -Tvec[1];
+    position[2] = +Tvec[2];
 
+    // now calculare orientation quaternion
+    cv::Mat Rot(3, 3, CV_32FC1);
+    cv::Rodrigues(Rvec, Rot);
 
+    // calculate axes for quaternion
+    double stAxes[3][3];
+    // x axis
+    stAxes[0][0] = -Rot.at< double >(0, 0);
+    stAxes[0][1] = -Rot.at< double >(1, 0);
+    stAxes[0][2] = +Rot.at< double >(2, 0);
+    // y axis
+    stAxes[1][0] = -Rot.at< double >(0, 1);
+    stAxes[1][1] = -Rot.at< double >(1, 1);
+    stAxes[1][2] = +Rot.at< double >(2, 1);
+    // for z axis, we use cross product
+    stAxes[2][0] = stAxes[0][1] * stAxes[1][2] - stAxes[0][2] * stAxes[1][1];
+    stAxes[2][1] = -stAxes[0][0] * stAxes[1][2] + stAxes[0][2] * stAxes[1][0];
+    stAxes[2][2] = stAxes[0][0] * stAxes[1][1] - stAxes[0][1] * stAxes[1][0];
 
+    // transposed matrix
+    double axes[3][3];
+    axes[0][0] = stAxes[0][0];
+    axes[1][0] = stAxes[0][1];
+    axes[2][0] = stAxes[0][2];
+
+    axes[0][1] = stAxes[1][0];
+    axes[1][1] = stAxes[1][1];
+    axes[2][1] = stAxes[1][2];
+
+    axes[0][2] = stAxes[2][0];
+    axes[1][2] = stAxes[2][1];
+    axes[2][2] = stAxes[2][2];
+
+    // Algorithm in Ken Shoemake's article in 1987 SIGGRAPH course notes
+    // article "Quaternion Calculus and Fast Animation".
+    double fTrace = axes[0][0] + axes[1][1] + axes[2][2];
+    double fRoot;
+
+    if (fTrace > 0.0) {
+        // |w| > 1/2, may as well choose w > 1/2
+        fRoot = sqrt(fTrace + 1.0); // 2w
+        orientation[0] = 0.5 * fRoot;
+        fRoot = 0.5 / fRoot; // 1/(4w)
+        orientation[1] = (axes[2][1] - axes[1][2]) * fRoot;
+        orientation[2] = (axes[0][2] - axes[2][0]) * fRoot;
+        orientation[3] = (axes[1][0] - axes[0][1]) * fRoot;
+    } else {
+        // |w| <= 1/2
+        static unsigned int s_iNext[3] = {1, 2, 0};
+        unsigned int i = 0;
+        if (axes[1][1] > axes[0][0])
+            i = 1;
+        if (axes[2][2] > axes[i][i])
+            i = 2;
+        unsigned int j = s_iNext[i];
+        unsigned int k = s_iNext[j];
+
+        fRoot = sqrt(axes[i][i] - axes[j][j] - axes[k][k] + 1.0);
+        double *apkQuat[3] = {&orientation[1], &orientation[2], &orientation[3]};
+        *apkQuat[i] = 0.5 * fRoot;
+        fRoot = 0.5 / fRoot;
+        orientation[0] = (axes[k][j] - axes[j][k]) * fRoot;
+        *apkQuat[j] = (axes[j][i] + axes[i][j]) * fRoot;
+        *apkQuat[k] = (axes[k][i] + axes[i][k]) * fRoot;
+    }
+}
 
 
 
